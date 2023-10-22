3e0d3495b92c1a022478b5bc685898fd524455e3
Womby <realwombynator@gmail.com>
2023-10-22 23:01:30 +0200

    tested parallax and new categories

--- js/index.js
+++ js/index.js
@@ -4,10 +4,10 @@ accordions.forEach(accordion => {
     accordion.addEventListener('click', function () {
         this.classList.toggle('accordion-active');
         const panel = this.nextElementSibling;
-        if (panel.style.display === 'block') {
+        if (panel.style.display === 'flex') {
             panel.style.display = 'none';
         } else {
-            panel.style.display = 'block';
+            panel.style.display = 'flex';
         }
     });
 });
